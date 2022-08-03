bbd036a26c271bae21cd51152d6843e92293aae1
massimocibeira <cibeiramassimo@gmail.com>
2022-08-02 21:13:19 -0300

    ase

---
+++ php/contact_form.php
@@ -0,0 +1,75 @@
+<?php
+
+	$to = 'madodesign.contacto@gmail.com';  // please change this email id
+	
+	$errors = array();
+	// print_r($_POST);
+
+	// Check if name has been entered
+	if (!isset($_POST['name'])) {
+		$errors['name'] = 'Please enter your name';
+	}
+	
+	// Check if email has been entered and is valid
+	if (!isset($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+		$errors['email'] = 'Email invalido';
+	}
+	
+	//Check if message has been entered
+	if (!isset($_POST['message'])) {
+		$errors['message'] = 'Escribir mensaje';
+	}
+
+	$errorOutput = '';
+
+	if(!empty($errors)){
+
+		$errorOutput .= '<div class="alert alert-danger alert-dismissible" role="alert">';
+ 		$errorOutput .= '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+		$errorOutput  .= '<ul>';
+
+		foreach ($errors as $key => $value) {
+			$errorOutput .= '<li>'.$value.'</li>';
+		}
+
+		$errorOutput .= '</ul>';
+		$errorOutput .= '</div>';
+
+		echo $errorOutput;
+		die();
+	}
+
+
+
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+	$from = $email;
+	$subject = 'Contact Form : nombre de pag empresa';
+	
+	$body = "Nombre: $name\n E-Mail: $email\n Mensaje:\n $message";
+
+
+	//send the email
+	$result = '';
+	if (mail ($to, $subject, $body)) {
+		$result .= '<div class="alert alert-success alert-dismissible" role="alert">';
+		$result .= '¡Gracias! Estaré en contacto';
+		$result .= '<a href="../index.html" type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">Volver al sitio</span></a>';
+		$result .= '</div>';
+
+		echo $result;
+		die();
+	}
+
+	$result = '';
+	$result .= '<div class="alert alert-danger alert-dismissible" role="alert">';
+	$result .= '<button href="index.html" type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+	$result .= 'Algo malo sucede durante el envío de este mensaje. Por favor, inténtelo de nuevo más tarde';
+	$result .= '</div>';
+
+	echo $result;
+	die();
+
+
+?>
